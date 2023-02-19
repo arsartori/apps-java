54867fdf93c2351bbf8ff0901a26f305146b605e
Andre Sartori <pathux.dev@outlook.com>
2023-02-19 18:59:56 -0300

    Update app

--- 7.0/src/main/java/com/aph/app1/App1Application.java
+++ 7.0/src/main/java/com/aph/app1/App1Application.java
@@ -2,9 +2,10 @@ package com.aph.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App1Application {
+public class App1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App1Application.class, args);
---
+++ 8.0/src/main/java/com/aph/app1/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.aph.app1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Demo Tomcat 8.0";
+    }
+}
